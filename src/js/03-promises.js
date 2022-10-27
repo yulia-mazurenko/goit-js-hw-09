15ff3b706c92f784d61b0f6ef604b8ec7348b01f
yulia-mazurenko <yulia.mazurenko1@gmail.com>
2022-10-28 02:04:57 +0300

    update files

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,5 @@
-// import Notiflix from 'notiflix';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 let getEl = selector => document.querySelector(selector);
 
