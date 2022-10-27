9419e66ccf25d325aca9a4a6b266f6b3b8bd6b6f
yulia-mazurenko <106391487+yulia-mazurenko@users.noreply.github.com>
2022-10-27 22:49:33 +0000

    Deploying to gh-pages from @ yulia-mazurenko/goit-js-hw-09@8adb70e8f170c28ae621356b3f4738a429f81607 🚀

---
+++ 01-color-switcher.8ef123ec.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let n=null;t.startBtn.addEventListener("click",(function(){n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.stopBtn.disabled=!0,t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.8ef123ec.js.map
